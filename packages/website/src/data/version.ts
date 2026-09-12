4f7fe8520deaad568b9761fa502f4452afcaed9f
danidetenerife <12203716+danidetenerife@users.noreply.github.com>
2026-09-12 14:52:08 +0100

    auto: sync updates 2026-09-12 14:52:08

--- packages/website/src/data/version.ts
+++ packages/website/src/data/version.ts
@@ -1,4 +1,4 @@
-export const version = '1.48.24';
+export const version = '1.48.25';
 export const versionTag = `v${version}`;
 export const releaseTag = `v${version}`;
 export const releaseUrl = (filename: string) =>
