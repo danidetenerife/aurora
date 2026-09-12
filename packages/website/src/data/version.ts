0966136cb1d14409728aff8a9de900f814506c30
danidetenerife <12203716+danidetenerife@users.noreply.github.com>
2026-09-12 22:12:05 +0100

    player@1.48.44

--- packages/website/src/data/version.ts
+++ packages/website/src/data/version.ts
@@ -1,4 +1,4 @@
-export const version = '1.48.43';
+export const version = '1.48.44';
 export const versionTag = `v${version}`;
 export const releaseTag = `v${version}`;
 export const releaseUrl = (filename: string) =>
